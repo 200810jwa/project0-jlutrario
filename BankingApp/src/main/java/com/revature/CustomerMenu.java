4e5833dbb02784b68ca0d3fc3b866e7b642e975c
Jayson Lutrario <lutrarioo@aol.com>
2020-08-22 23:52:45 -0400

    completed customer UI

--- BankingApp/src/main/java/com/revature/CustomerMenu.java
+++ BankingApp/src/main/java/com/revature/CustomerMenu.java
@@ -2,6 +2,8 @@ package com.revature;
 
 import java.util.Scanner;
 
+import com.revature.dao.IUserDAO;
+import com.revature.dao.UserDAO;
 import com.revature.models.Account;
 import com.revature.models.Application;
 import com.revature.models.User;
@@ -85,26 +87,44 @@ public class CustomerMenu {
 					break;
 				default:
 					System.out.println("|------------- NOT AN OPTION -------------|");
+					System.out.println("|------------ TRY AGAIN LATER ------------|");
 					System.out.println("|                                         |");
 				}
 			} else {	// application found
 				System.out.println("| Application Status:                     |");
-				System.out.print  ("| id: " + app.getId());
+				System.out.println("| id: " + app.getId());
 				if (app.isActive()) {
 					System.out.println("| status: PENDING APPROVAL                |");
+					System.out.println("|                                         |");
 				} else {
 					System.out.println("| status: DENIED                          |");
+					System.out.println("|                                         |");
+					System.out.println("| Want to remove this application?        |");
+					System.out.println("| [1] Yes                                 |");
+					System.out.println("| [2] No                                  |");
+					System.out.print  ("| Choose an option: ");
+					String option = scan.nextLine();
+					
+					switch (option) {
+					case "1":
+						if (appService.removeApplication(app)) {
+							System.out.println("|---------- APPLICATION REMOVED ----------|");
+							System.out.println("|                                         |");
+						} else {
+							System.out.println("|------ COULD NOT REMOVE APPLICATION -----|");
+							System.out.println("|                                         |");
+						}
+						break;
+					case "2":
+						break;
+					default:
+						System.out.println("|------------- NOT AN OPTION -------------|");
+						System.out.println("|                                         |");
+					}
 				}
 			}
 		} else {	// account found
-			System.out.print  ("| id: " + a.getId());
-			System.out.print  ("| balance: $" + a.getBalance());
-			System.out.print  ("| owner: " + a.getOwner().getFirstname() + " " + a.getOwner().getLastname());
-			System.out.println("|   [1] Withdraw                          |");
-			System.out.println("|   [2] Deposit                           |");
-			System.out.println("|   [3] Transfer                          |");
-			System.out.println("|   [4] Close Account                     |");
-			System.out.println("|   [5] Back                              |");
+			transactionMenu(a);
 		}
 		
 		scan.close();
@@ -121,7 +141,7 @@ public class CustomerMenu {
 			System.out.print  ("| Enter Current Password: ");
 			String curr_pass = scan.nextLine();
 			attempt++;
-			if (curr_pass.equals(u.getPassword())) {
+			if (u.getPassword().equals(curr_pass)) {
 				System.out.print  ("| Enter New Password: ");
 				String new_pass = scan.nextLine();
 				userService.changePassword(u.getId(), new_pass);
@@ -140,6 +160,129 @@ public class CustomerMenu {
 		if (attempt == 3) {
 			System.out.println("|----------- TOO MANY ATTEMPTS -----------|");
 			System.out.println("|------------ TRY AGAIN LATER ------------|");
+			System.out.println("|                                         |");
+		}
+		
+		scan.close();
+	}
+	
+	private void transactionMenu(Account a) {
+		Scanner scan = new Scanner(System.in);
+		AccountService accountService = new AccountService();
+		IUserDAO userDAO = new UserDAO();
+		
+		System.out.println("|                                         |");
+		System.out.print  ("| id: " + a.getId());
+		System.out.print  ("| balance: $" + a.getBalance());
+		System.out.print  ("| owner: " + a.getOwner().getFirstname() + " " + a.getOwner().getLastname());
+		System.out.println("|   [1] Withdraw                          |");
+		System.out.println("|   [2] Deposit                           |");
+		System.out.println("|   [3] Transfer                          |");
+		System.out.println("|   [4] Close Account                     |");
+		System.out.println("|   [5] Back                              |");
+		System.out.print  ("|   Choose an action: ");
+		String action = scan.nextLine();
+		
+		switch (action) {
+		case "1":
+			System.out.println("|--------------- WITHDRAW ----------------|");
+			System.out.println("|                                         |");
+			System.out.print  ("| current balance: $" + a.getBalance());
+			System.out.println("| Enter withdraw amount: ");
+			double wAmount = Double.parseDouble(scan.nextLine());
+			if (accountService.withdraw(a, wAmount)) {
+				System.out.println("|--------- WITHDRAW SUCCESSFUL! ----------|");
+				System.out.println("|                                         |");
+			} else {
+				System.out.println("|------- COULD NOT WITHDRAW FUNDS --------|");
+				System.out.println("|                                         |");
+			}
+			break;
+		case "2":
+			System.out.println("|---------------- DEPOSIT ----------------|");
+			System.out.println("|                                         |");
+			System.out.print  ("| current balance: $" + a.getBalance());
+			System.out.println("| Enter deposit amount: ");
+			double dAmount = Double.parseDouble(scan.nextLine());
+			System.out.println("|                                         |");
+			
+			if (accountService.deposit(a, dAmount)) {
+				System.out.println("|---------- DEPOSIT SUCCESSFUL! ----------|");
+				System.out.println("|                                         |");
+			} else {
+				System.out.println("|-------- COULD NOT DEPOSIT FUNDS --------|");
+				System.out.println("|                                         |");
+			}
+			break;
+		case "3":
+			System.out.println("|---------------- TRANSFER ---------------|");
+			System.out.println("|                                         |");
+			System.out.print  ("| current balance: $" + a.getBalance());
+			System.out.println("| Enter amount to transfer: ");
+			double tAmount = Double.parseDouble(scan.nextLine());
+			System.out.println("| Enter target's username: ");
+			String username = scan.nextLine();
+			User targetUser = userDAO.findByUsername(username);
+			if (targetUser != null) {
+				Account target = accountService.findAccount(targetUser);
+				if (target != null) {
+					if (accountService.transfer(a, target, tAmount)) {
+						System.out.println("|---------- TRANSFER SUCCESSFUL! ---------|");
+						System.out.println("|                                         |");
+					} else {
+						System.out.println("|------- COULD NOT TRANSFER FUNDS --------|");
+						System.out.println("|                                         |");
+					}
+				} else {
+					System.out.println("|--------- COULD NOT FIND ACCOUNT --------|");
+					System.out.println("|                                         |");
+				}
+			} else {
+				System.out.println("|---------- COULD NOT FIND USER ----------|");
+				System.out.println("|                                         |");
+			}
+			break;
+		case "4":
+			System.out.println("|------------- CLOSE ACCOUNT -------------|");
+			System.out.println("|                                         |");
+			System.out.println("| Are you sure?                           |");
+			System.out.println("| [1] Yes                                 |");
+			System.out.println("| [2] No                                  |");
+			System.out.print  ("| Enter choice: ");
+			String choice = scan.nextLine();
+			
+			switch (choice) {
+			case "1":
+				System.out.print  ("| Enter password: ");
+				System.out.println("|                                         |");
+				String pw = scan.nextLine();
+				if (u.getPassword().equals(pw)) {
+					if (a.getBalance() != 0) {
+						System.out.println("|--------- ACCOUNT MUST BE EMPTY ---------|");
+						System.out.println("|                                         |");
+					} else if (accountService.closeAccount(a)) {
+						System.out.println("|------ ACCOUNT SUCCESSFULLY CLOSED ------|");
+						System.out.println("|                                         |");
+					} else {
+						System.out.println("|-------- COULD NOT CLOSE ACCOUNT --------|");
+						System.out.println("|                                         |");
+					}
+				} else {
+					System.out.println("|----------- INVALID PASSWORD ------------|");
+					System.out.println("|                                         |");
+				}
+				break;
+			case "2":
+				break;
+			default:
+				System.out.println("|------------- NOT AN OPTION -------------|");
+				System.out.println("|                                         |");
+			}
+			break;
+		case "5":
+			break;
+		default:
+			System.out.println("|------------- NOT AN OPTION -------------|");
 			System.out.println("|                                         |");
 		}
 		
